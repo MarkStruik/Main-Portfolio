050fe1e17a0a0486a008862d438e376a3a389209
Mark Struik <mark.struik@outlook.com>
2022-11-11 11:29:58 +0000

    added tailwind and headless ui

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-function HomePage() {
-  return <div>Welcome to my dev page. <br/> currently under construction. </div>
-}
-
-export default HomePage
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function HomePage() {
+  return <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen p-12">
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex flex-col items-center space-x-4">
+    <div>
+      Welcome to my dev page.</div>
+    <div>currently under construction.</div>
+    </div>
+  </div>
+}
+
+export default HomePage
